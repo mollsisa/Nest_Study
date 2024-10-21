5e65a75c55d7199f4a16d82527d8b9504d78df89
moll <moll@alunos.utfpr.edu.br>
2024-10-21 12:21:17 -0300

    FEAT: Create bookmark functions

--- crud_study/src/bookmark/bookmark.service.ts
+++ crud_study/src/bookmark/bookmark.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+    constructor(private prisma: PrismaService) {}
+    getBookmarks(userId: number) {
+        return this.prisma.bookmark.findMany({
+            where: {
+                userId: userId
+            }
+        });
+    }
+
+    getBookmarkById(userId: number, bookmarkId: number) {
+        return this.prisma.bookmark.findFirst({
+            where: {
+                id: bookmarkId,
+                userId: userId
+            }
+        });
+    }
+
+    async createBookmark(userId: number, dto: CreateBookmarkDto) {
+        const bookmark = await this.prisma.bookmark.create({
+            data: {
+                userId,
+                ...dto
+            }
+        });
+
+        return bookmark;
+    }
+
+    async editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto) {
+        const bookmark = await this.prisma.bookmark.findUnique({
+            where: {
+                id: bookmarkId
+            }
+        });
+        if (!bookmark || bookmark.userId !== userId) {
+            throw new ForbiddenException('Bookmark not found');
+        }
+
+        return this.prisma.bookmark.update({
+            where: {
+                id: bookmarkId
+            },
+            data: {
+                ...dto
+            }
+        });
+    }
+
+    deleteBookmarkById(userId: number, bookmarkId: number) {
+
+    }
+}
